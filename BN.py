b5ca7982b9b085d9481767c28ec0ef828a1cb026
Afonso Luís <44056464+AfonsoMALuis@users.noreply.github.com>
2018-12-04 21:55:26 +0000

    BN quase feito

--- BN.py
+++ BN.py
@@ -13,26 +13,58 @@ class Node():
         self.prob=prob
     
     def computeProb(self, evid):
-        if len(self.parents)<2:
-            pos=self.prob
-            neg=1-self.prob
-            result = [neg[0], pos[0]]
+        if len(self.parents)==0:
+            pos=self.prob.item(0)
+            neg=1-self.prob.item(0)
+            result = [neg, pos]
             return result
-        else:
-            
-            return 0
+        elif len(self.parents)==1:
+            if evid[self.parents[0]]==0:
+                pos=self.prob.item(0)
+                neg=1-self.prob.item(0)
+                result = [neg, pos]
+                return result
+            elif evid[self.parents[0]]==1:
+                pos=self.prob.item(1)
+                neg=1-self.prob.item(1)
+                result = [neg, pos]
+                return result 
+        elif len(self.parents)==2:
+            if evid[self.parents[0]]==0 and evid[self.parents[1]]==0:
+                pos=self.prob.item(0)
+                neg=1-self.prob.item(0)
+                result = [neg, pos]
+                return result
+            elif evid[self.parents[0]]==0 and evid[self.parents[1]]==1:
+                pos=self.prob.item(1)
+                neg=1-self.prob.item(1)
+                result = [neg, pos]
+                return result 
+            elif evid[self.parents[0]]==1 and evid[self.parents[1]]==0:
+                pos=self.prob.item(2)
+                neg=1-self.prob.item(2)
+                result = [neg, pos]
+                return result 
+            elif evid[self.parents[0]]==1 and evid[self.parents[1]]==1:
+                pos=self.prob.item(3)
+                neg=1-self.prob.item(3)
+                result = [neg, pos]
+                return result             
     
 class BN():
     def __init__(self, gra, prob):
-        pass
+        self.gra=gra
+        self.prob=prob
 
     def computePostProb(self, evid):
-        pass
-               
-        return 0
+        for i in range(len(evid)):
+            if evid[i]==-1:
+                return 0
         
         
     def computeJointProb(self, evid):
-        pass
-        
-        return 0
+        soma=1
+        if evid!=(0,0,0,0,0):
+            for i in range(len(self.prob)):
+                soma*=self.prob[i].computeProb(evid)[1]
+        return soma
